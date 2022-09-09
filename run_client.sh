bb1f614d9b384ef57920bd9f56db67d9381aef98
Jaroslaw Przerywacz <jaroslaw.przerywacz@ericsson.com>
2022-09-09 15:00:16 +0200

    Added language option
    Change-Id: I402d2da7ecf97d4295f5609893847020044077e0

--- run_client.sh
+++ run_client.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 cp util/run.escript _build/default/lib/communicator/ebin/
 cd _build/default/lib/communicator/ebin
-./run.escript
+
+echo "Select your language"
+echo "pl -polski"
+echo "en -english"
+read -p "Option: " lang
+
+if [ "$lang" = "en" ]; then
+	./run.escript en
+fi
+
+if [ "$lang" = "pl" ]; then
+	./run.escript pl
+fi
+
+$SHELL
--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 cp server_config.txt _build/default/lib/communicator/ebin/
-cd _build/default/lib/communicator/ebin
-erl -eval 'communicator:start_link().'
+cp prompts _build/default/lib/communicator/ebin/
+
+echo "Select your language"
+echo "pl -polski"
+echo "en -english"
+read -p "Option: " lang
+
+if [ "$lang" = "en" ]; then
+	erl -pa '_build/default/lib/communicator/ebin' -eval 'communicator:start_link(en).'
+fi
+
+if [ "$lang" = "pl" ]; then
+	erl -pa '_build/default/lib/communicator/ebin' -eval 'communicator:start_link(pl).'
+fi
+
+echo "Wrong language"
+$SHELL
+
