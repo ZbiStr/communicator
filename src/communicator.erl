c4678288791e5cd5e99e68002a70e5fe95165e47
Krzysztof Dwuznik <krzysztof.dwuznik@ericsson.com>
2022-09-19 14:47:09 +0200

    Fixed afk time + serverName
    Change-Id: I81d5efbbe87b2e7f0bdc08160fa00d4074f4f50f

--- src/communicator.erl
+++ src/communicator.erl
@@ -32,10 +32,10 @@
 ]).
 
 -define(SERVER, ?MODULE).
--define(NODE_NAME, dzialajkupo).
+-define(NODE_NAME, erlangTcpServer).
 -define(COOKIE, ciasteczko).
 -define(MSG_DELIVERY_TIME, 10000).
--define(AFK_TIME, 1000).
+-define(AFK_TIME, 120000).
 
 -record(state, {
     server_name = undefined,
